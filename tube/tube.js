bfddef5bdd02591ce0e8e39b2b05fe75d6d3f309
KouIshihara <as.it.were.kou@gmail.com>
2017-05-01 11:25:48 +0900

    tote

--- tube/tube.js
+++ tube/tube.js
@@ -1,4 +1,4 @@
-var BranchTube = function(_pos, _points, _branchNum){
+var BranchTube = function(_pos, _points, _euler, _branchNum){
   // コンストラクタ
   this.points = _points;
   this.branchNum = _branchNum;
@@ -10,28 +10,74 @@ var BranchTube = function(_pos, _points, _branchNum){
   material.color = new THREE.Color( 0x999999 );
   material.wireframe = true;
   material.wireframeLinewidth = 0.1;
-  console.log(material);
   this.mesh = new THREE.Mesh(tubeGeometry, material);
   this.mesh.position.x = _pos.x;
   this.mesh.position.y = _pos.y;
   this.mesh.position.z = _pos.z;
+  //this.mesh.rotation.x = _euler.x;
+  //this.mesh.rotation.z = _euler.z;
   this.mesh.branchNum = _branchNum;
 
+  //console.log(_euler.x);
+  //console.log(_euler.z);
+  //console.log(this.mesh.rotation.x);
+
+  // 四方向のみの分岐
+  
+  var testRan;
+  while(true){
+    testRan = (Math.random()*2-1.0)*20;
+    if (testRan>4 || -4>testRan) break;
+  }
   var test = Math.random()-0.5;
-  var testRan = ( Math.random() * ( ( 20 + 1 ) - 4 ) ) + 4;
+
+  if(test < 0){
+    this.mesh.rotation.x += Math.PI/testRan; 
+    //console.log('x '+testRan);
+  }else{
+    this.mesh.rotation.z += Math.PI/testRan;
+    //console.log('z '+testRan);
+  }
+  //console.log('z '+this.mesh.rotation.z/Math.PI);
+  
+
+  /*
+  var testRan;
+  while(true){
+    testRan = (Math.random()*2-1.0)*20;
+    if (testRan>4 || -4>testRan) break;
+  }
+  var test = Math.random()-0.5;
+
+  var testRan = (Math.random()*((20+1)-4)) + 4; // 4~20
   if(test<0){
+    this.mesh.rotation.x = Math.PI/testRan;
+  }else{
+    this.mesh.rotation.x = -Math.PI/testRan;
+  }
+
+  var test3 = Math.random()-0.5;
+  if(test3<0){
     this.mesh.rotation.z = Math.PI/testRan;
   }else{
     this.mesh.rotation.z = -Math.PI/testRan;
   }
+  */
+
   /*
   var test2 = Math.random()-0.5;
   if(test2<0){
-    this.mesh.rotation.x = Math.PI/testRan;
+    this.mesh.rotation.y = Math.PI/testRan;
   }else{
-    this.mesh.rotation.x = -Math.PI/testRan;
-  }*/
+    this.mesh.rotation.y = -Math.PI/testRan;
+  }
+  */
+
   
+  
+  //this.mesh.rotation.x = Math.PI/6;
+  //this.mesh.rotation.z = Math.PI/6;
+
   this.nextPos = {x: 0, y: 0, z:0};
   this.testIs = true;
 }
@@ -42,21 +88,49 @@ BranchTube.prototype.getMesh = function(){
 
 BranchTube.prototype.update = function(){
   if (this.points[1].y < this.maxHeight) {
-    this.points[1].y += 0.1;
-    this.radius += 0.002; // これifの処理わけてもいいかもね
+    this.points[1].y += 0.4;
+    this.radius += 0.008; // これifの処理わけてもいいかもね
     this.mesh.geometry = new THREE.TubeGeometry(new THREE.CatmullRomCurve3(this.points), 32, this.radius, 8, false);
-    this.nextPos.x = this.mesh.position.x - this.points[1].y*Math.sin(this.mesh.rotation.z);
-    //this.nextPos.y = this.mesh.position.y + this.points[1].y*Math.cos(this.mesh.rotation.x);
-    this.nextPos.y = this.mesh.position.y + this.points[1].y*Math.cos(this.mesh.rotation.z);
-    //this.nextPos.z = this.mesh.position.z + this.points[1].y*Math.cos(this.mesh.rotation.y);
-    this.nextPos.z = 0;
+    
+    //this.nextPos.x = this.mesh.position.x - this.points[1].y*Math.sin(this.mesh.rotation.z);
+    //this.nextPos.y = this.mesh.position.y + this.points[1].y*Math.cos(this.mesh.rotation.z);
+    
+    var r = this.maxHeight;
+    var theta = Math.PI/2 - this.mesh.rotation.x;
+    var phi = Math.PI/2 - this.mesh.rotation.z;
+    
+    this.nextPos.x = this.mesh.position.x + -r*Math.sin(theta)*Math.cos(phi); // Good
+    this.nextPos.y = this.mesh.position.y + r*Math.sin(theta)*Math.sin(phi); // Good
+    this.nextPos.z = this.mesh.position.z + r*Math.cos(theta); // theta
+    
+
+    /*
+    var angle = this.mesh.rotation.x;
+    var angle2 = this.mesh.rotation.z;
+    this.nextPos.x = Math.cos(angle) * Math.cos(angle2) * r;
+    this.nextPos.y = Math.cos(angle) * Math.sin(angle2) * r;
+    this.nextPos.z = Math.sin(angle) * r;
+    */
   }else{
     if(this.testIs){
-      console.log(this.points[1].y);
+      //console.log(this.points[1].y);
       this.testIs = false;
+      //console.log(this.nextPos);
+      //console.log(this.mesh);
+      /*
+      var axes = new THREE.AxisHelper(20);
+      axes.position.x = this.nextPos.x;
+      axes.position.y = this.nextPos.y;
+      axes.position.z = this.nextPos.z;      
+      scene.add(axes);
+      */
     }
   }
-  //this.mesh.rotation.y += 0.1;
+  /*
+  if(this.mesh.rotation.y < 2.0*Math.PI){
+    this.mesh.rotation.y += 0.1;
+  }
+  */
 }
 
 var scene;
@@ -82,14 +156,14 @@ function init() {
   webGLRenderer.shadowMap.enabled = true;
 
   camera.position.z = 200;
-  camera.lookAt(new THREE.Vector3(0, 20, 0));
+  camera.lookAt(new THREE.Vector3(0, 0, 0));
 
   var trackballControls = new THREE.TrackballControls(camera);
-  trackballControls.rotateSpeed = 1.0;
-  trackballControls.zoomSpeed = 1.0;
-  trackballControls.panSpeed = 1.0;
+  trackballControls.rotateSpeed = 2.0;
+  trackballControls.zoomSpeed = 2.0;
+  trackballControls.panSpeed = 2.0;
   trackballControls.staticMoving = true;
-  trackballControls.target.set(0,40,0);
+  trackballControls.target.set(0,20,0);
 
 
   var projector = new THREE.Projector();
@@ -102,7 +176,8 @@ function init() {
   var points = [];
   points.push(new THREE.Vector3(0, 0, 0));
   points.push(new THREE.Vector3(0, 0, 0));
-  var branch = new BranchTube(pos, points, branches_array.length);
+  var euler = THREE.Euler;
+  var branch = new BranchTube(pos, points, euler, branches_array.length);
   branches_array.push(branch);
   scene.add(branch.getMesh());
 
@@ -139,8 +214,9 @@ function init() {
       points.push(new THREE.Vector3(0, 0, 0));
       points.push(new THREE.Vector3(0, 0, 0));
       var branchNum = branches_array.length;
-      console.log(pos);
-      var branch = new BranchTube(pos, points, branchNum);
+      console.log(mesh.rotation);
+      var euler = mesh.rotation;
+      var branch = new BranchTube(pos, points, euler, branchNum);
       branches_array.push(branch);
       scene.add(branches_array[branchNum].getMesh());
     }else{
